2d7d763f691fea13aa92f7fd7257113f852403c1
islo123 <43340489+islo123@users.noreply.github.com>
2019-01-29 14:33:42 +0200

    Updates

---
+++ precache-manifest.cc3e6a34f2f36ca5f60277d1bf3f646a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hearts-for-sports-brain-for-education/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8ddcc777697b56893071",
+    "url": "/hearts-for-sports-brain-for-education/static/js/runtime~main.8ddcc777.js"
+  },
+  {
+    "revision": "7ca8e18259f270999499",
+    "url": "/hearts-for-sports-brain-for-education/static/js/main.7ca8e182.chunk.js"
+  },
+  {
+    "revision": "d0d69ec9ccd7b21a3f2d",
+    "url": "/hearts-for-sports-brain-for-education/static/js/1.d0d69ec9.chunk.js"
+  },
+  {
+    "revision": "7ca8e18259f270999499",
+    "url": "/hearts-for-sports-brain-for-education/static/css/main.08128059.chunk.css"
+  },
+  {
+    "revision": "69830a25dd919d7ed908c44b94f321be",
+    "url": "/hearts-for-sports-brain-for-education/index.html"
+  }
+];
